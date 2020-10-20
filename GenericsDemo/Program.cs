1773a141a81935038591575c608635966735d79b
KUMMARI TIRUPATHI RAO <35338095+ktr-hub@users.noreply.github.com>
2020-10-20 09:15:19 +0530

    [Tirupathi] Add. Generics Method

--- GenericsDemo/Program.cs
+++ GenericsDemo/Program.cs
@@ -11,37 +11,24 @@ namespace GenericsDemo
             double[] doubleArray = { 1.1, 2.2, 3.3, 4.4 };
             char[] charArray = { 'A', 'B', 'C', 'D' };
 
-            //Calling methods
+            //Calling methods for UC-2
             Console.WriteLine("Int Array elements :");
-            toPrint(intArray);
+            toPrint<int>(intArray);
             Console.WriteLine("Double Array elements :");
-            toPrint(doubleArray);
+            toPrint<double>(doubleArray);
             Console.WriteLine("Char Array elements :");
-            toPrint(charArray);
+            toPrint<char>(charArray);
+
+
         }
         //To print Int Array
-        public static void toPrint(int[] inputArray)
+        public static void toPrint<T>(T[] inputArray)
         {
-            foreach(int element in inputArray)
+            foreach(T element in inputArray)
             {
                 Console.WriteLine(element);
             }
         }
-        //To print Double Array
-        public static void toPrint(double[] inputArray)
-        {
-            foreach (double element in inputArray)
-            {
-                Console.WriteLine(element);
-            }
-        }
-        //To print Char Array
-        public static void toPrint(char[] inputArray)
-        {
-            foreach (char element in inputArray)
-            {
-                Console.WriteLine(element);
-            }
-        }
+        
     }
 }
